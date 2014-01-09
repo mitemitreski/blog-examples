6a483cea73dfdc5f05dd64805c3c55e1d82eb6a6
Mite Mitreski <mmitreski@tricode.nl>
2014-01-09 02:16:28 +0100

    spaces for indent and added example for usage of partials

--- mustache-java/mustache-spring-mvc/src/main/java/com/mitemitreski/blog/HomeController.java
+++ mustache-java/mustache-spring-mvc/src/main/java/com/mitemitreski/blog/HomeController.java
@@ -1,12 +1,5 @@
 package com.mitemitreski.blog;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Properties;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -14,33 +7,40 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.text.DateFormat;
+import java.util.*;
+
 /**
  * Hello Mustache.
  */
 @Controller
 public class HomeController {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(HomeController.class);
+  private static final Logger logger = LoggerFactory
+      .getLogger(HomeController.class);
 
 
-	/**
-	 * Simple controller that redirects to home and adds map and date objects.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+  /**
+   * Simple controller that redirects to home and adds map and date objects.
+   */
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String home(Locale locale, Model model) {
 
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
-				DateFormat.LONG, locale);
+    Properties properties = System.getProperties();
 
-		Properties properties = System.getProperties();
+    Map<String, String> map = new HashMap<String, String>((Map) properties);
+    String formattedDate = currentTime(locale);
+    model.addAttribute("serverTime", formattedDate);
+    model.addAttribute("props", map.entrySet());
+    return "home";
+  }
 
-		Map<String, String> map = new HashMap<String, String>((Map) properties);
 
-		String formattedDate = dateFormat.format(date);
-		model.addAttribute("serverTime", formattedDate);
-		model.addAttribute("props", map.entrySet());
-		return "home";
-	}
+
+  private String currentTime(Locale locale) {
+    Date date = new Date();
+    DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
+        DateFormat.LONG, locale);
+    return dateFormat.format(date);
+  }
 }
